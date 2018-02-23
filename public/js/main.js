6d707f9adc52c8be7cdfbbfba93b7591351196db
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2018-02-23 12:08:04 +0600

    Added:Submenu on hover Contact Form

--- public/js/main.js
+++ public/js/main.js
@@ -95,8 +95,13 @@ jQuery(document).ready(function($) {
     		}
     	}
     	
-    });  
-	 
+    });
+
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
 	  
 	//COUNTDOWN TIMER
 	var newYear = new Date(); 
