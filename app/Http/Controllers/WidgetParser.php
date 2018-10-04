eb31d49f8209b3ebd91ad8702296d25383414a1a
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2018-10-04 18:11:19 +0600

    Team people widget development

--- app/Http/Controllers/WidgetParser.php
+++ app/Http/Controllers/WidgetParser.php
@@ -870,9 +870,8 @@ class WidgetParser extends Controller
             }
             foreach ($team_people as $person)
             {
-
                 $pInfo = SitePeople::getPersonInfo($person);
-                dump(asset('uploads/'.$pInfo));
+                dump(asset($pInfo));
                 $ret.= '<div class="col-lg-'.$size.' col-md-3 col-sm-4"><div class="team-sin text-center">';
 
                 $ret .= '<div class="speaker-img">
