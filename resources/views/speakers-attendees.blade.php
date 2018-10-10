8066176ace4d9d9c1014783093087c13d110458f
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2018-10-10 06:39:20 +0000

    New pages and Uploads

---
+++ resources/views/speakers-attendees.blade.php
@@ -0,0 +1,20 @@
+@extends($template)
+                
+                            @section('header-extra')
+                                        
+                            @endsection
+                                        
+                            @section('title')
+                               {{$page_title}}
+                             @endsection
+                                        
+                                        
+                            @section('page-content')
+                                        
+                                {!! $data !!}
+                                        
+                            @endsection
+                                        
+                            @section('scripts')
+                                        
+                            @endsection
